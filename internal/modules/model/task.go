f41c482017743307e13da64cd004f577116fbd0d
Cailiang <1006980793@qq.com>
2025-06-05 14:58:50 +0800

    修复 sql error

--- internal/modules/model/task.go
+++ internal/modules/model/task.go
@@ -11,7 +11,7 @@ type Task struct {
 	Model        string      `json:"model" gorm:"column:model;type:varchar(20)"`
 	Quality      string      `json:"quality" gorm:"column:quality;type:varchar(20)"`
 	Size         string      `json:"size" gorm:"column:size;type:varchar(20)"`
-	Status       string      `json:"status" gorm:"column:status;type:enum('pending', 'queued', 'running', 'succeed', 'aborted', failed')"`
+	Status       string      `json:"status" gorm:"column:status;type:enum('pending', 'queued', 'running', 'succeed', 'aborted', 'failed')"`
 	FailedReason string      `json:"failed_reason" gorm:"column:failed_reason;type:varchar(1000)"`
 	Progress     float32     `json:"progress" gorm:"column:progress;type:float"`
 	CreatedAt    time.Time   `json:"created_at" gorm:"column:created_at;type:datetime;not null;default:CURRENT_TIMESTAMP"`
