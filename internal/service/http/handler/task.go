34c52dfffef3b607a5857a29d230e458b9a8615c
lizj <lizj@shuzilm.cn>
2025-11-27 10:34:58 +0800

    nano-banana-2

--- internal/service/http/handler/task.go
+++ internal/service/http/handler/task.go
@@ -141,7 +141,7 @@ func (h *TaskHandler) edit(ctx context.Context) {
 			TaskID:     h.task.Id,
 		}
 		gpt.NewProvider(ctx, []observer.Observer{h}).FastSpeed(editRequest)
-	} else if strings.HasPrefix(h.task.Model, "gemini") || strings.HasPrefix(h.task.Model, "nano-banana") {
+	} else if strings.HasPrefix(h.task.Model, "gemini") {
 		req := gemini.Request{
 			ImageBytes: bs,
 			Prompt:     h.task.Prompt,
@@ -195,7 +195,7 @@ func (h *TaskHandler) generate(ctx context.Context) {
 			TaskID:  h.task.Id,
 		}
 		gpt.NewProvider(ctx, []observer.Observer{h}).FastSpeed(editRequest)
-	} else if strings.HasPrefix(h.task.Model, "gemini") || strings.HasPrefix(h.task.Model, "nano-banana") {
+	} else if strings.HasPrefix(h.task.Model, "gemini") {
 		req := gemini.Request{
 			Prompt: h.task.Prompt,
 			Model:  h.task.Model,
